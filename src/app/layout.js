1e6c0806a00316e8b89914b636948889fa410b70
Priyanka Kumari <priyankacoder11@gmail.com>
2024-11-20 17:36:05 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ToastContainer, toast } from 'react-toastify';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+<ToastContainer>
         {children}
+        </ToastContainer>
       </body>
     </html>
   );
